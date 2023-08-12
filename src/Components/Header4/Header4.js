55c0b4e1520a5af4f3e9897536c14e165d1ad080
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-12 17:29:09 +0330

    create header4

--- src/Components/Header4/Header4.js
+++ src/Components/Header4/Header4.js
@@ -3,7 +3,14 @@ import './Header4.css'
 export default function Header4() {
     return (
         <div className='header4'>
-
+            <div className='header4__left'>
+                <div className='text_box'>
+                    <h1 className='title'>Place Place!</h1>
+                    <h1 className='sub__title'>Place Place!</h1>
+                    <p className='txt'></p>
+                </div>
+            </div>
+            <div className='header4__right'>this is right</div>
         </div>
     )
 }
