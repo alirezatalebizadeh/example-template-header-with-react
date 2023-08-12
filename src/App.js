33957022067bb0c19e243945aec44a1f20ecacb9
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-08-12 11:24:24 +0330

    add header1 to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar';
+import Header1 from './Components/Header1/Header1';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header1/>
     </div>
   );
 }
---
+++ src/Components/Header1/Header1.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header1.css'
+export default function Header1() {
+    return (
+        <div class="container">
+            <h1 class="text">
+                Creative Header <br />
+                Design
+            </h1>
+            <div class="header">
+                <div class="header_left"></div>
+                <div class="header_right">
+                    <h2 class="wellcome">Wellcome</h2>
+                    <p class="paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum culpa
+                        consequuntur tempore aperiam libero voluptatibus, nemo modi maiores
+                        id magni quo non, eveniet sequi blanditiis excepturi fugiat, nam
+                        porro quaerat.
+                    </p>
+                    <button class="btn">Get Started</button>
+                </div>
+            </div>
+        </div>
+    )
+}
